b1bee137ba58b64d8b9e8607e90e97e5b259309b
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2023-08-19 23:49:58 +0530

    tut32 - #32 Static Site Generation (SSG) in Next.js 13.4

--- tut2/src/app/page.js
+++ tut2/src/app/page.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
         <main>
-            <h1>Export Static HTML Page with Build.</h1>
+            <h1>SSG in Next js </h1>
         </main>
     );
 }
---
+++ tut2/src/app/users/page.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import getUsers from "../../../services/getUsers";
+
+export default async function Page() {
+    const getUserList = getUsers();
+    const users = await getUserList;
+    console.log(users);
+    console.log("hello");
+    return (
+        <div>
+            <h1>User List</h1>
+            {users.map((user) => (
+                <h2 key={user.id}>
+                    <Link href={`/users/${user.id}`}>{user.name}</Link>
+                </h2>
+            ))}
+        </div>
+    );
+}
